72616095b61764229d117c60cc4de4d5c995e6b4
EC2 Default User <ec2-user@ip-172-31-85-41.ec2.internal>
2020-03-03 15:00:52 +0000

    setup graphql-client

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import { ApolloProvider } from 'react-apollo'
+import ggl from 'graphql-tag'
+import { Query } from 'react-apollo'
+import client from './client'
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-:console.log({GITHUB_TOKEN})
-
-function App() {
-  return (
-    <div>Hi,GraphQL
-    </div>
+const ME = ggl`
+	query me {
+		user(login:"iteachonudemy"){
+			name
+			avatarUrl
+		}
+	}
+`
+class App extends React.Component  {
+	render() {
+  return(
+	  <ApolloProvider client={client}>
+	  	<div>Hi,GraphQL</div>
+	  	<Query query={ME}>
+	  		{
+				({ loading, error,data}) =>{
+					if (loading) return 'Loading..'
+					if (error) return `Error! ${error.message}`
+					return <div>{data.user.name}</div>
+				}
+			}
+	  	</Query>
+	  </ApolloProvider>
   )
+}
 }
 
 export default App
