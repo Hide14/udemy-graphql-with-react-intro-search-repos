fccf74d9a3a793ca738cdd1d9b919c2a0497a9c0
EC2 Default User <ec2-user@ip-172-31-85-41.ec2.internal>
2020-03-04 06:51:49 +0000

    apply query and variables

--- src/graphql.js
+++ src/graphql.js
@@ -1,5 +1,33 @@
 import ggl from 'graphql-tag'
 
+
+export const SEARCH_REPOSITORIES = ggl`
+  query searchRepositries($first:Int,$after:String,$before:String,$last:Int,$query:String!){
+      search(first:$first,before:$before,after:$after,last:$last,query:$query,type:REPOSITORY){
+        pageInfo{
+          endCursor
+          hasNextPage
+          hasPreviousPage
+          startCursor
+        }
+        repositoryCount
+        edges{
+          node{
+            ... on Repository{
+              id
+              name
+              url
+              stargazers{
+                totalCount
+              }
+              viewerHasStarred
+            }
+          }
+          cursor
+        }
+      }
+    }
+`
 export const ME = ggl`
 	query me {
 		user(login:"iteachonudemy"){
